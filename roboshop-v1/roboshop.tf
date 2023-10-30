70b30055b6ce0ede6eac879fe90972a97d360f74
mahipal <mahipal.muchkurr@gmail.com>
2023-10-30 15:09:16 +0530

    Password NFR

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "frontend-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -17,6 +25,15 @@ resource "aws_instance" "mongodb" {
     Name = "mongodb"
   }
 }
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "mongodb-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
+}
+
 
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
@@ -28,6 +45,15 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "catalogue-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue.private_ip]
+}
+
+
 resource "aws_instance" "redis" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -37,6 +63,15 @@ resource "aws_instance" "redis" {
     Name = "redis"
   }
 }
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "redis-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+}
+
 
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
@@ -48,6 +83,15 @@ resource "aws_instance" "user" {
   }
 }
 
+resource "aws_route53_record" "user" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "user-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user.private_ip]
+}
+
+
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -56,6 +100,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "cart-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -68,6 +120,14 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "mysql-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -76,6 +136,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "shipping-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -87,6 +155,14 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "rabbitmq-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 
 resource "aws_instance" "payment" {
   ami           = "ami-03265a0778a880afb"
@@ -98,6 +174,15 @@ resource "aws_instance" "payment" {
   }
 }
 
+resource "aws_route53_record" "payment" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "payment-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.payment.private_ip]
+}
+
+
 resource "aws_instance" "dispatch" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -106,4 +191,12 @@ resource "aws_instance" "dispatch" {
   tags = {
     Name = "dispatch"
   }
+}
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z036242411JCZSVIGSVSH"
+  name    = "dispatch-dev.mdevopsb74.online"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.dispatch.private_ip]
 }

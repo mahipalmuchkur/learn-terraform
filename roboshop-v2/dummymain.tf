e1c3ce4a5b3a0d016b6e7f2a90130ff48e187b29
mahipal <mahipal.muchkurr@gmail.com>
2023-11-03 20:01:01 +0530

    Password NFR

--- roboshop-v2/dummymain.tf
+++ roboshop-v2/dummymain.tf
@@ -2,50 +2,102 @@ variable "ami" {
   default = "ami-03265a0778a880afb"
 }
 
-variable "instance_type" {
+variable "instance" {
   default = "t3.small"
 }
 
-variable "security_groups" {
+variable "security_group" {
   default = [ "sg-08a14ab1911d86db6" ]
 }
 
 variable "zone_id" {
   default = "Z036242411JCZSVIGSVSH"
 }
-
 variable "components" {
-  default = {
-    frontend    = { name = "frontend" }
-    mongodb     = { name = "mongodb" }
-    catalogue   = { name = "catalogue" }
-    redis       = { name = "redis" }
-    user        = { name = "user" }
-    cart        = { name  = "cart" }
-    mysql       = { name = "mysql" }
-    shipping    = { name = "shipping" }
-    rabbitmq    = { name = "rabbitmq" }
-    payment     = { name = "payment" }
-  }
+  frontend  = { name = "frontend-dev" }
+  mongodb   = { name = "mongodb-dev" }
+  catalogue = { name = "catalogue-dev" }
+  redis     = { name = "redis-dev" }
+  user      = { name = "user-dev" }
+  cart      = { cart = "cart-dev" }
+  mysql     = {name = "mysql-dev" }
+  shipping  = {name = "shipping-dev" }
+  rabbitmq  = { name = "rabbitmq-dev" }
+  payment   = { name = "payment" }
+  dispatch  = {name = "dispatch" }
+
 }
 
 resource "aws_instance" "instance" {
-  for_each               = var.components
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  vpc_security_group_ids = var.security_groups
-
+  for_each = var.components
+  ami           = var.ami
+  instance_type = var.instance
+  vpc_security_group_ids = var.security_group
 
   tags = {
-    Name = lookup(each.value, "name", null )
+    Name = lookup(each.value,name, null)
   }
 }
 
 resource "aws_route53_record" "record" {
   for_each = var.components
-  zone_id  = var.zone_id
-  name     = "${lookup(each.value, "name", null )}.mdevopsb74.online"
-  type     = "A"
-  ttl      = "30"
-  records  = [lookup(lookup(aws_instance.instance, each.key, null ), "private_ip", null)]
+  zone_id = var.zone_id
+  name    = "${lookup(each.value,name, null)}.mdevopsb74.online"
+  type    = "A"
+  ttl     = "30"
+  records    = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip", null)]
 }
+
+
+#
+#variable "ami" {
+#  default = "ami-03265a0778a880afb"
+#}
+#
+#variable "instance_type" {
+#  default = "t3.small"
+#}
+#
+#variable "security_groups" {
+#  default = [ "sg-08a14ab1911d86db6" ]
+#}
+#
+#variable "zone_id" {
+#  default = "Z036242411JCZSVIGSVSH"
+#}
+#
+#variable "components" {
+#  default = {
+#    frontend    = { name = "frontend" }
+#    mongodb     = { name = "mongodb" }
+#    catalogue   = { name = "catalogue" }
+#    redis       = { name = "redis" }
+#    user        = { name = "user" }
+#    cart        = { name  = "cart" }
+#    mysql       = { name = "mysql" }
+#    shipping    = { name = "shipping" }
+#    rabbitmq    = { name = "rabbitmq" }
+#    payment     = { name = "payment" }
+#  }
+#}
+#
+#resource "aws_instance" "instance" {
+#  for_each               = var.components
+#  ami                    = var.ami
+#  instance_type          = var.instance_type
+#  vpc_security_group_ids = var.security_groups
+#
+#
+#  tags = {
+#    Name = lookup(each.value, "name", null )
+#  }
+#}
+#
+#resource "aws_route53_record" "record" {
+#  for_each = var.components
+#  zone_id  = var.zone_id
+#  name     = "${lookup(each.value, "name", null )}.mdevopsb74.online"
+#  type     = "A"
+#  ttl      = "30"
+#  records  = [lookup(lookup(aws_instance.instance, each.key, null ), "private_ip", null)]
+#}

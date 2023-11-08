16bfcf53e80eb891b777b09572143be722b3e002
mahipal <mahipal.muchkurr@gmail.com>
2023-11-08 14:42:08 +0530

    Password NFR

--- robo-v3/module/main.tf
+++ robo-v3/module/main.tf
@@ -15,3 +15,14 @@ resource "aws_route53_record" "dev-ns" {
   ttl     = "30"
   records = [aws_instance.instance.private_ip]
 }
+
+resource "null_resource" "ansible" {
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/learn-ansible
+git pull
+sleep 30
+ansible-palybook -i ${var.name}-dev.mdevopsb74.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
+    EOF
+  }
+}

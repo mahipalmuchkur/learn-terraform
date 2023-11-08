0468e41db3ceec076c2b3bfc0903614bbab30ce8
mahipal <mahipal.muchkurr@gmail.com>
2023-11-08 15:00:10 +0530

    Password NFR

--- robo-v3/module/main.tf
+++ robo-v3/module/main.tf
@@ -17,6 +17,8 @@ resource "aws_route53_record" "dev-ns" {
 }
 
 resource "null_resource" "ansible" {
+
+  depends_on = [aws_route53_record]
   provisioner "local-exec" {
     command = <<EOF
 cd /home/centos/learn-ansible
